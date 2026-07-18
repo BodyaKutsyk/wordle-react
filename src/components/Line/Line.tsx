87fda657cb84c25a2d3ba2efee33119eef3c9692
BodyaKutsyk <mkkbodya@gmail.com>
2026-07-18 23:09:29 +0300

    fix line cell highlight with multiple chars

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -1,19 +1,17 @@
-import { forwardRef, useEffect } from 'react';
+import { forwardRef, useMemo } from 'react';
 import { CharStatus } from '../../types/CharStatus';
 import { Tile } from '../Tile';
 import lineStyles from './Line.module.scss';
-import tileStyles from '../Tile/Tile.module.scss';
 import React from 'react';
 import getCharStatus from '../../utils/get-char-status';
 import { useWord } from '../../providers/word-provider';
 
 type Props = {
-  line: null | string[];
+  line: string[];
   correctWord: string;
   attempt: number;
   index: number;
   isGameOver: boolean;
-  isReseted: boolean;
 };
 
 const charPriority: Record<CharStatus, number> = {
@@ -25,35 +23,62 @@ const charPriority: Record<CharStatus, number> = {
 
 export const Line = React.memo(
   forwardRef<HTMLDivElement, Props>(
-    ({ line, correctWord, attempt, index, isGameOver, isReseted }, ref) => {
+    ({ line, correctWord, attempt, index, isGameOver }, ref) => {
       const { setUsedChars, usedChars } = useWord();
       const isPassed = !!attempt && attempt > index;
 
       const validRef = attempt === index ? ref : null;
-      let notRepeated = '';
 
-      useEffect(() => {
-        line?.forEach(tile => {
-          document
-            .getElementById(tile)
-            ?.classList.remove(
-              tileStyles.correct,
-              tileStyles.missed,
-              tileStyles.wrong,
-            );
-        });
-      }, [isReseted, line]);
+
+      // TODO: simplify and test this shit
+      const lineHighlights = useMemo(() => {
+        const lineStatus: CharStatus[] = new Array(5).fill('not-used');
+        const chars: Record<string, number> = {};
+        const correctChars: Record<string, number> = {};
+
+        for (const char of correctWord) {
+          if (correctChars[char]) {
+            correctChars[char]++;
+          } else {
+            correctChars[char] = 1;
+          }
+        }
+
+        for (let i = 0; i < line?.length; i++) {
+          let status: CharStatus = "not-used";
+          let tile = line?.[i];
+
+          if (!chars[tile]) {
+            chars[tile] = 1;
+          } else {
+            chars[tile]++;
+          }
+
+          if (isPassed && !isGameOver) {
+            status = getCharStatus(tile, i, correctWord);
+
+            if (chars[tile] >= correctChars[tile]) {
+              const prevSiblingIndex = line.indexOf(tile);
+
+              if (status === 'correct') {
+                lineStatus[prevSiblingIndex] = 'wrong';
+              } else if (
+                (status === 'missed' && lineStatus[prevSiblingIndex] === 'missed') || (status === 'missed' && lineStatus[prevSiblingIndex] === 'correct')
+              ) {
+                status = 'wrong';
+              }
+            }
+
+            lineStatus[i] = status;
+          }
+        }
+        return lineStatus;
+      }, [correctWord, line, isPassed, isGameOver])
 
       return (
         <div ref={validRef} className={lineStyles.line}>
           {line?.map((tile, i) => {
-            let status: CharStatus = 'not-used';
-
-            if (isPassed && !notRepeated.includes(tile) && !isGameOver) {
-              status = getCharStatus(tile, i ,correctWord);
-
-              notRepeated += tile;
-            }
+            const status = lineHighlights[i];
 
             if (charPriority[status] > charPriority[usedChars.get(tile) || 'not-used'] || !usedChars.has(tile)) {
               setUsedChars(prev => {
